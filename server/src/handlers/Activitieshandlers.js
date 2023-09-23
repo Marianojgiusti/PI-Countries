447fade4dd6de2b8adb7de6d24baac40ef2fcb7a
Marianojgiusti <giusti_mariano@hotmail.com>
2023-09-23 16:42:15 -0300

    hola

--- server/src/handlers/Activitieshandlers.js
+++ server/src/handlers/Activitieshandlers.js
@@ -17,7 +17,7 @@ const CreateActivitiesHandler = async (req, res) => {
         res.status(200).json(newActivity)
     }
     catch (error) {
-        res.status(400).json({error: error.message})
+        res.status(404).json({error:error.message})
     }
 }
 
